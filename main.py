5d5da0c5ab0c02db74d456d834214fa6cdf46056
Elisa N <117632231+iminiom@users.noreply.github.com>
2022-11-18 17:28:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import pyxel
 class Jeu:
     def __init__(self):
         pyxel.init(128, 128)
-        self.vaisseau_x = 60
-        self.vaisseau_y = 60
+        self.plateau_x = 60
+        self.plateau_y = 60
         pyxel.run(self.update, self.draw)
 
     def vaisseau_deplacement(self):
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.vaisseau_x<120:
-            self.vaisseau_x += 1
-        if pyxel.btn(pyxel.KEY_LEFT) and self.vaisseau_x>0:
-            self.vaisseau_x += -1
-        if pyxel.btn(pyxel.KEY_DOWN) and self.vaisseau_y<120:
+        if pyxel.btn(pyxel.D) and self.plateau_x<120:
+            self.plateau_x += 1
+        if pyxel.btn(pyxel.Q) and self.plateau_x>0:
+            self.plateau_x += -1
+        if pyxel.btn(pyxel.S) and self.plateau_y<120:
             self.vaisseau_y += 1
-        if pyxel.btn(pyxel.KEY_UP) and self.vaisseau_y>0:
-            self.vaisseau_y += -1
+        if pyxel.btn(pyxel.Z) and self.plateau_y>0:
+            self.plateau_y += -1
 
     def update(self):
-        self.vaisseau_deplacement()
+        self.plateau_deplacement()
 
     def draw(self):
         pyxel.cls(0)
